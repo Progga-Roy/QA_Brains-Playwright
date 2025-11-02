e8fc77786f80033c375ab28ee3af8e4540f8113f
Progga-Roy <proggaroy2000@gmail.com>
2025-11-02 19:16:49 +0600

    Create file and fetch title

---
+++ playwright.config.js
@@ -0,0 +1,30 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout:30000,
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+    headless: false,       // show the browser
+    viewport: null,        // disables fixed viewport
+    // launchOptions: {
+    //   args: ['--start-maximized'], // starts browser maximized
+    // },
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+  ],
+
+});
+
---
+++ tests/homePage.spec.js
@@ -0,0 +1,11 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('Fetch title', async ({ page }) => {
+  await page.goto('https://qabrains.com/');
+  await page.setViewportSize({width:1528,height:1080});
+  await page.waitForTimeout(2000);
+  await expect(page.locator("//h2[contains(text(),'The Leading Community for Quality Assurance and So')]")).toHaveText('The Leading Community for Quality Assurance and Software Testing');
+
+}); 
+
