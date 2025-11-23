054b5a95cd13a80fb01cc722f806a658ac4b9d93
Progga-Roy <proggaroy2000@gmail.com>
2025-11-23 19:47:57 +0600

    Complete user registration

---
+++ tests/registration.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('User Registration', async({page})=>{
+    await page.goto("https://practice.qabrains.com/");
+    await page.click("//span[normalize-space()='Registration']");
+    const resigtrationTitle = page.locator("//h2[normalize-space()='User Authentication']");
+    await expect(resigtrationTitle).toHaveText("User Authentication");
+    await page.fill("#name", "Jesica")
+    await page.selectOption("#country","Bangladesh");
+    await page.selectOption("#account","Student");
+    await page.fill("#email","qa_testers@qabrains.com");
+    await page.fill("#password","Password123");
+    await page.fill("#confirm_password","Password123");
+    const submitBtn = page.locator("button[type='submit']")
+    await expect(submitBtn).toBeEnabled();
+    await submitBtn.click();
+    const regSuccessMsg = page.locator(".mb-10.text-right");
+    await expect(regSuccessMsg).toHaveText("Registration Successful");
+
+})
